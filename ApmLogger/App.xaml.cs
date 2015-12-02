d200c54005e49d21bba49a39b81feee722d7112b
Jinah Adam <jinahadam@gmail.com>
2015-12-02 16:10:49 +0800

    Log APM data to console

---
+++ ApmLogger/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace ApmLogger
+{
+    
+    public partial class App : Application
+    {
+  
+     
+
+    }
+
+}
